72ebeae1d748ef23bab9c77d101d92f354f67d88
Kksshh123 <gim180371@gmail.com>
2023-02-08 08:24:55 +0900

    한국어 test

--- app/src/main/java/com/example/hackerton/PostFragment.kt
+++ app/src/main/java/com/example/hackerton/PostFragment.kt
@@ -1,6 +1,8 @@
 package com.example.hackerton
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -19,12 +21,29 @@ class PostFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentPostBinding.inflate(layoutInflater)
+        binding = FragmentPostBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        
+        settingTextCnt()
+    }
+
+    private fun settingTextCnt() {
+        binding.etTitle.addTextChangedListener(object: TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                binding.txtCnt.text = "${binding.etTitle.length()} / 15"
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+
+            }
+
+        })
     }
 }
--- app/src/main/java/com/example/hackerton/SplashActivity.kt
+++ app/src/main/java/com/example/hackerton/SplashActivity.kt
@@ -4,24 +4,20 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.widget.Button
 import android.widget.ImageButton
+import com.example.hackerton.databinding.SplashBackgroundBinding
 
 
 class SplashActivity : AppCompatActivity() {
-
-    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.splash_background)
-        val intent = Intent(this, LoginActivity::class.java)
-        val loginbtn = findViewById<ImageButton>(R.id.splash)
 
-        loginbtn.setOnClickListener {
-            startActivity(intent)
-        }
-    }
-    companion object {
-        private const val DURATION : Long = 2000
+        Handler().postDelayed({
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }, 1500)
     }
 }
