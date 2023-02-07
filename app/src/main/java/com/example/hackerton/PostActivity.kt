b9bc8ac01094c839106bae9bfb0cab63d0b0b336
Kksshh123 <gim180371@gmail.com>
2023-02-08 00:41:10 +0900

    post text rimit

--- app/src/main/java/com/example/hackerton/PostActivity.kt
+++ app/src/main/java/com/example/hackerton/PostActivity.kt
@@ -3,28 +3,47 @@ package com.example.hackerton
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.addTextChangedListener
+import com.example.hackerton.databinding.ActivityPstBinding
 
 class PostActivity: AppCompatActivity(){
-    @SuppressLint("MissingInflatedId")
+
+    private lateinit var binding: ActivityPstBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_pst)
+        binding = ActivityPstBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         val intent = Intent(this, ActivityMainWindowMainActivity::class.java)
-
-        val title = findViewById<EditText>(R.id.Title)
-        val writing = findViewById<EditText>(R.id.writing)
-        val post = findViewById<Button>(R.id.Post)
 
         var Writing = ""
         var Title = ""
 
-        Writing = writing.text.toString()
-        Title = title.text.toString()
+        Writing = binding.writing.text.toString()
+        Title = binding.Title.text.toString()
 
-        post.setOnClickListener {
+        binding.Title.addTextChangedListener(object : TextWatcher {
+            var maxText = ""
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                maxText = p0.toString()
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                binding.txtCnt.text = "${binding.Title.length()} / 10"
+
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+
+            }
+
+        })
+
+        binding.Post.setOnClickListener {
             startActivity(intent)
         }
     }
