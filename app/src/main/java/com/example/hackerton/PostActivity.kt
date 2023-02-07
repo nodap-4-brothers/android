c0fd0ce663da5fdf84d1df7e5f5a4383326700b1
Kksshh123 <gim180371@gmail.com>
2023-02-08 07:56:47 +0900

    bcabcabadfs

--- app/src/main/java/com/example/hackerton/PostActivity.kt
+++ app/src/main/java/com/example/hackerton/PostActivity.kt
@@ -1,14 +1,11 @@
 package com.example.hackerton
 
-import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import android.widget.Button
-import android.widget.EditText
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.widget.addTextChangedListener
 import com.example.hackerton.databinding.ActivityPstBinding
 
 class PostActivity: AppCompatActivity(){
@@ -24,16 +21,21 @@ class PostActivity: AppCompatActivity(){
         var Title = ""
 
         Writing = binding.writing.text.toString()
-        Title = binding.Title.text.toString()
+        Title = binding.etTitle.text.toString()
 
-        binding.Title.addTextChangedListener(object : TextWatcher {
+        var cnt = ""
+        Log.d("testasd", "1234")
+
+        binding.etTitle.addTextChangedListener(object : TextWatcher {
             var maxText = ""
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
                 maxText = p0.toString()
             }
 
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                binding.txtCnt.text = "${binding.Title.length()} / 10"
+                cnt = binding.etTitle.text.toString()
+                Log.d("testasd", "$cnt d")
+                binding.txtCnt.text = "${cnt.length}/15"
 
             }
 
