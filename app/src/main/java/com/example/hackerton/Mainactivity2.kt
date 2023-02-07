ba7725dcb2741f7f9efad10764975bdb26faa9d7
Kksshh123 <gim180371@gmail.com>
2023-02-07 18:42:39 +0900

    main2 password hide or show

--- app/src/main/java/com/example/hackerton/Mainactivity2.kt
+++ app/src/main/java/com/example/hackerton/Mainactivity2.kt
@@ -4,30 +4,55 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.text.Editable
+import android.text.InputType
 import android.text.TextWatcher
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.ToggleButton
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity2 : AppCompatActivity() {
+
+    private val PwToggle: ImageView by lazy {
+        findViewById(R.id.eye)
+    }
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
-        val backBtn = findViewById<ImageButton>(R.id.backbutton)
+
         val id = findViewById<EditText>(R.id.Id)
         val login = findViewById<Button>(R.id.login)
         val password = findViewById<EditText>(R.id.Password)
+        val checkPw = findViewById<EditText>(R.id.password2)
         val intent = Intent(this, LoginActivity::class.java)
 
         var idmessege = ""
         var pwmessage = ""
 
         login.isEnabled = false
-        backBtn.setOnClickListener {
-            finish()
+
+        PwToggle.setImageResource(R.drawable.hide)
+        password.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+        checkPw.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+
+        PwToggle.setOnClickListener {
+            if(PwToggle.tag.equals("0")) {
+                PwToggle.tag = "1"
+                PwToggle.setImageResource(R.drawable.shape)
+
+                password.inputType = InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD
+                checkPw.inputType = InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD
+            } else if (PwToggle.tag.equals("1")) {
+                PwToggle.tag = "0"
+                PwToggle.setImageResource(R.drawable.hide)
+
+                password.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+                checkPw.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+            }
         }
         id.addTextChangedListener(/* watcher = */ object: TextWatcher{
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
